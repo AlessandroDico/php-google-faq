1cd816a99d65f5bcb0919a3dc474a5245eb7c00f
Ale <alessandrochb@gmail.com>
2020-12-16 18:50:12 +0100

    aggiunta fav icon

--- index.php
+++ index.php
@@ -83,6 +83,8 @@ $faq = [
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <!-- font awesome -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+        <!-- favicon -->
+        <link rel="shortcut icon" href="https://ssl.gstatic.com/policies/favicon.ico">
     </head>
     <body>
         <header>
